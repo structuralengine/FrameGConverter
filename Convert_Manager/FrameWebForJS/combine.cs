a50f03229a47efabc4b57090de106492b2987d1e
sasaco <sasaicco@gmail.com>
2022-08-23 10:59:53 +0900

    defineが完成してcombine になった

---
+++ Convert_Manager/FrameWebForJS/combine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Convert_Manager.FrameWebForJS
+{
+    public class Combine
+    {
+        public string name;
+        public Dictionary<string, double> coef = new Dictionary<string, double>();
+    }
+
+    class combine
+    {
+    }
+}
--- Convert_Manager/FrameWebForJS/define.cs
+++ Convert_Manager/FrameWebForJS/define.cs
@@ -21,27 +21,33 @@ namespace Convert_Manager.FrameWebForJS
         public define(Dictionary<string, string> wdata)
         {
             /// 番号
-            if (wdata.ContainsKey(define.wFile1))
+            if (!wdata.ContainsKey(define.wFile1))
+                return;
+            if (!wdata.ContainsKey(define.wFile2))
+                return;
+
+            var str1 = wdata[define.wFile1];
+            var str2 = wdata[define.wFile2];
+
+            while (str1.Length > 9)
             {
-                var str = wdata[define.wFile1];
-                while (str.Length > 9)
+                var tmp = comon.byteSubstr(ref str1, 10);
+                var no = tmp.Trim();
+
+                string str = comon.byteSubstr(ref str2, 180);
+
+                var df = new Define();
+
+                for (int i = 0; i < 30; i++)
                 {
-                    var tmp = comon.byteSubstr(ref str, 10);
-                    var no = (0 < tmp.Length) ? Convert.ToInt32(tmp) : -1;
-
+                    tmp = comon.byteSubstr(ref str, 6).Trim();
+                    if (0 < tmp.Length)
+                    {
+                        df.CaseNumbers.Add("C"+(i+1), Convert.ToInt32(tmp));
+                    }
                 }
+                DefineList.Add(no, df);
             }
-
-            /// 内容
-            if (wdata.ContainsKey(define.wFile2))
-            {
-                var str = wdata[define.wFile2];
-
-            }
-
-
-
-
         }
 
 
@@ -49,15 +55,17 @@ namespace Convert_Manager.FrameWebForJS
         {
             var result = new Dictionary<string, Dictionary<string, int>>();
 
+            int row = 1;
             foreach(var a in DefineList)
             {
                 var b = new Dictionary<string, int>();
-                b.Add("row", a.Value.row);
+                b.Add("row", row);
                 foreach(var c in a.Value.CaseNumbers)
                 {
                     b.Add(c.Key, c.Value);
                 }
                 result.Add(a.Key, b);
+                row++;
             }
 
             return result;
